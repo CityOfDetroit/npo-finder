1a6a8cc04c2df4187deb028cb567c82244bd7c8e
Edgar Montes <jedgar1mx@gmail.com>
2021-04-28 13:47:18 -0400

    Rebuilding NPO finder withoug mapbox, update NPO service.

--- index.js
+++ index.js
@@ -1,30 +1,11 @@
 'use strict';
-import './node_modules/@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import './scss/styles.scss';
 import Controller from './js/controller.class.js';
 (function(){
-  let controller = new Controller({
-    styleURL: 'mapbox://styles/mapbox',
-    mapContainer: 'map',
-    geocoder: true,
-    controls: false,
-    draw: false,
-    baseLayers: {
-      street: 'streets-v10',
-      satellite: 'cj774gftq3bwr2so2y6nqzvz4'
-    },
-    center: [-83.10, 42.36],
-    zoom: 10.75,
-    boundaries: {
-      sw: [-83.3437,42.2102],
-      ne: [-82.8754,42.5197]
-    },
-    sources: [],
-    layers: []
-  });
+  let controller = new Controller();
   document.getElementById('clear-services-btn').addEventListener('click', function(){
     document.getElementById('npo-results').className = '';
-    document.querySelector('.mapboxgl-ctrl-geocoder.mapboxgl-ctrl input').value = '';
     controller.loadDefault(controller);
+    controller.geocoder.form.reset();
   });
 })(window);
--- js/controller.class.js
+++ js/controller.class.js
@@ -1,14 +1,14 @@
 'use strict';
-import Map from './map.class.js';
-import Panel from './panel.class.js';
-import DataManager from './data-manager.class.js';
+import Geocoder from './geocoder.class';
+import Panel from './panel.class';
+import DataManager from './data-manager.class';
 export default class Controller {
-  constructor(map) {
+  constructor() {
     this.tempNPO = null;
     this.allNPOs = null;
     this.panel = new Panel();
     this.dataManager = new DataManager();
-    this.map = new Map(map, this);
+    this.geocoder = new Geocoder('geocoder', this);
     this.initialLoad(this);
   }
   initialLoad(controller){
@@ -16,35 +16,6 @@ export default class Controller {
   }
   loadDefault(controller){
     controller.panel.creatPanel('all', controller);
-  }
-  geocoderResults(e, controller){
-    let tempAddr = e.result.place_name.split(",");
-    tempAddr = tempAddr[0];
-    tempAddr = tempAddr.split(" ");
-    let newTempAddr = '';
-    let size = tempAddr.length;
-    tempAddr.forEach(function(item, index) {
-      newTempAddr += item;
-      ((index < size) && (index + 1) !== size) ? newTempAddr += '+': 0;
-    });
-    // console.log(newTempAddr);
-    let url = "https://gis.detroitmi.gov/arcgis/rest/services/DoIT/CompositeGeocoder/GeocodeServer/findAddressCandidates?Street=&City=&ZIP=&SingleLine=" + newTempAddr + "&category=&outFields=User_fld&maxLocations=&outSR=4326&searchExtent=&location=&distance=&magicKey=&f=json";
-    fetch(url)
-    .then((resp) => resp.json()) // Transform the data into json
-    .then(function(data) {
-      // console.log(data);
-      if(data.candidates.length){
-        if(data.candidates[0].attributes.User_fld != ""){
-          // console.log('parcel found');
-          controller.dataManager.buildData(data.candidates[0], controller);
-          // controller.panel.creatPanel("parcel", data.candidates[0].attributes.User_fld, controller);
-        }else{
-          // console.log("no parcel found");
-        }
-      }else{
-        // console.log("no parcel found");
-      }
-    });
   }
   closeAlert(ev){
     (ev.target.parentNode.parentNode.id === 'alert-overlay') ? document.getElementById('alert-overlay').className = '': document.getElementById('drill-down-overlay').className = '';
